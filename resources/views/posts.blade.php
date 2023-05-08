4f9739b0e372f7dc28b384b38a43a91d0b092195
Aditya <adityadixit425@gmail.com>
2023-05-09 00:07:12 +0530

    09/05/2023

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./app.css">
+    {{-- <script src="./app.js"></script> --}}
+    <title>My Blog</title>
+</head>
+<body>
+    <article>
+        <h1><a href="/post/my-first-post">My First Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quasi dolore quis aliquam, nihil illum! Ut exercitationem dolore expedita veniam non repudiandae natus aliquam unde, laboriosam, iure iusto ratione pariatur?Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, sit necessitatibus aut dolore amet magni cupiditate vero iste qui facere praesentium aliquam modi ea ratione voluptates, nam iusto officia! Architecto.</p>
+    </article>
+    <article>
+        <h1><a href="/post/my-second-post">My Second Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quasi dolore quis aliquam, nihil illum! Ut exercitationem dolore expedita veniam non repudiandae natus aliquam unde, laboriosam, iure iusto ratione pariatur?Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, sit necessitatibus aut dolore amet magni cupiditate vero iste qui facere praesentium aliquam modi ea ratione voluptates, nam iusto officia! Architecto.</p>
+    </article>
+    <article>
+        <h1><a href="/post/my-third-post">My Third Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quasi dolore quis aliquam, nihil illum! Ut exercitationem dolore expedita veniam non repudiandae natus aliquam unde, laboriosam, iure iusto ratione pariatur?Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, sit necessitatibus aut dolore amet magni cupiditate vero iste qui facere praesentium aliquam modi ea ratione voluptates, nam iusto officia! Architecto.</p>
+    </article>
+</body>
+</html>
